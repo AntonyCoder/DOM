d33bad9549d1fba4c5badb5402a6933b44b45d09
Anton Lysenko <antonlysenko161@yandex.ru>
2025-03-23 22:41:32 +0300

    Скорректировал первое задание

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const body = document.querySelector('body');
+    const container = document.createElement('div');
+    container.classList.add('container');
+    
+    body.appendChild(container);
+
+    for (let i = 0; i < 16; i++) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        container.appendChild(cell);
+    }
+
     const cells = document.querySelectorAll('.cell');
 
     function getRandomInt() {
